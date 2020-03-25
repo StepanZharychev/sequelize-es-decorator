1b747188e9982b79ec57a11373fdc9a450845f5e
Stepan Zharychev <decadentsz@gmail.com>
2020-03-26 00:26:29 +0100

    Added indexing via batch operation.

--- methods/index.js
+++ methods/index.js
@@ -1,32 +1,22 @@
+require('array.prototype.flatmap').shim();
+
 module.exports = (model, client, database) => {
     const options = model.getSearchOptions();
 
     model.index = async () => {
         let entries = await model.findAll();
 
-        let totalLength = entries.length;
-        let processedLength = 0;
-
-        for (let index = 0; index < entries.length; index++) {
-            let entry = entries[index];
-            let body = {};
+        const body = entries.flatMap(entry => {
+            let doc = {};
 
             options.keys.map(key => {
-                body[key] = entry[key];
+                doc[key] = entry[key];
             });
 
-            await client.index({
-                index: `${database}_${options.type}`,
-                id: entry.id,
-                type: 'doc',
-                body
-            });
+            return [{ index: { _index: `${database}_${options.type}`, _id: entry.id } }, doc];
+        });
+        const { body: bulkResponse } = await client.bulk({ refresh: true, body });
 
-            processedLength++;
-
-            if (processedLength === totalLength) {
-                return processedLength;
-            }
-        }
+        return entries.length;
     };
 };
